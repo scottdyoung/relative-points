3ef5603e65376830b46f44de686823f0e76823d7
scott.young@issinc.com <scott.young@issinc.com>
2021-03-22 20:31:53 -0400

    removed test data

--- src/state/appState.ts
+++ src/state/appState.ts
@@ -7,28 +7,7 @@ export interface JiraState {
 }
 
 export const initialState: JiraState = {
-  jiraItems: [
-    {
-      id: "id-1",
-      key: '1',
-      title: "Foo Title 1"
-    },
-    {
-      id: "id-2",
-      key: '2',
-      title: "Foo Title 2"
-    },
-    {
-      id: "id-3",
-      key: '3',
-      title: "Foo Title 3"
-    },
-    {
-      id: "id-4",
-      key: '4',
-      title: "Foo Title 4"
-    }
-  ],
+  jiraItems: [],
   columns: [
     createNewColumn()
   ],
